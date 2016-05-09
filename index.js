c8f6996d7bcdafd8a9881b66686aee5c8628ff28
liuxiaoyu <306766053@qq.com>
2016-05-09 17:25:54 +0800

    add test case

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const EventEmitter = require('events')
+const debug = require('debug')('heartbeat')
 
 /**
  * follow num is default if not set
  * opts: {
  *  interval: 30
+ *  timeout: 2
  * }
  */
 module.exports = (opts) => {
@@ -13,20 +15,40 @@ module.exports = (opts) => {
 }
 class Heartbeat extends EventEmitter {
   constructor (opts) {
+    super()
     opts = opts || {}
     this.interval  = +opts.interval || 30
-    this.timer = null
+    this.timeout = +opts.timeout || 2 
+    this.intervalTimer = null
+    this.timeoutCurrentCount = 0
+    this.timeoutTimer = null
+    this.init()
+  }
+  
+  init () {
+    this.on('pong', () => {
+      this.timeoutCurrentCount = 0
+    })
+    this.on('ping', () => {
+      this.timeoutCurrentCount++
+    })
   }
   
   start () {
-    let heartbeat = this
-    this.timer = setInterval(() => {
-      heartbeat.emit('ping')
+    this.intervalTimer = setInterval(() => {
+      //if timeout count more than setting, emit timeout and stop heartbeat
+      debug(`${this.timeoutCurrentCount} ping`)
+      if(this.timeoutCurrentCount > this.timeout) {
+        this.emit('timeout')
+        this.stop()
+        return
+      }
+      this.emit('ping')
     }, this.interval * 1000)
   }
   
   stop () {
-    clearInterval(this.timer)
+    clearInterval(this.intervalTimer)
   }
   
   restart () {
--- test/index.js
+++ test/index.js
@@ -1,4 +1,31 @@
 'use strict'
-describe('', () => {
-  
+const heartbeat = require('..')
+const assert = require('assert')
+describe('', function () {
+  this.timeout(30 * 1000)  
+  it('no opts', () => {
+    let heart = heartbeat()
+    assert(heart.interval === 30)
+    assert(heart.timeout === 2)
+  })
+  it('with opts', () => {
+    let opts = {
+      interval: 300,
+      timeout: 3
+    }
+    let heart = heartbeat(opts)
+    assert(heart.interval === 300)
+    assert(heart.timeout === 3)
+  })
+  it('timeout', (done) => {
+    let opts = {
+      interval: 5,
+      timeout: 1
+    }
+    let heart = heartbeat(opts)
+    heart.on('timeout', () => {
+      done()
+    })
+    heart.start()
+  })
 })
